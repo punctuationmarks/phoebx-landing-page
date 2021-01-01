2bb962ca993a28eb8a85d20e47d8d2d3268af61a
punctuationmarks <punctuationmarks@protonmail.com>
2021-01-01 10:02:20 -0500

    Updated to be a bit more mobile friendly

--- src/sections/Services.js
+++ src/sections/Services.js
@@ -10,8 +10,8 @@ export default function Services(props) {
       </h2>
       <CardDeck>
         <CardComponent
-          cardTitle="Computer Technologies Consulting"
-          cardBodyText="We have experience in developing full stack web apps, wrangling and analyzing data, and building IoT devices. We 💖 JavaScript, Python, R, and Rust. If it runs on code, we have an interest in it."
+          cardTitle="Technologies Consulting"
+          cardBodyText="We have experience in developing full stack web apps, wrangling and analyzing data, and building IoT devices. We 💖 TypeScript, Python, R, and Rust. If it runs on code, we have an interest in it."
           buttonText="Github"
           buttonLink="https://www.github.com/punctuationmarks"
         />
@@ -20,8 +20,8 @@ export default function Services(props) {
           cardBodyText="With over 10 years of riding and building bikes, both professionally and as a hobby, we can find the right style and type of bike for you. We specialize in restoring vintage steel road, conversion, track, and touring bikes."
         />
         <CardComponent
-          cardTitle="Screen Printing (coming 2021)"
-          cardBodyText="We hit the sweet spot where fashion meets environmentally friendly, because you can be good to the climate and also look dress to the nines 👗 🥻. We take vintage clothes and give them a new life with custom designed screen printing. Every item is unique, measured properly for you to know your size, and custom made by artists, just like you."
+          cardTitle="Screen Printing"
+          cardBodyText="Aiming to reduce the waste from over consumption, we hit the sweet spot where fashion meets environmentally friendly. We take vintage clothes and give them a new life with custom designed screen printing, so you can be good to the climate and also look dress to the nines. Every item is unique, measured properly so you can actually know your size, and custom made by artists, just like you."
         />
       </CardDeck>
     </div>
